e7c1685794cc6ea17863e202ab575cab9481e171
Vincent Vermersch <vermersch@gmail.com>
2014-07-03 16:10:49 +0200

    merge failed

--- plugins/phpunit/phpunit.plugin.zsh
+++ plugins/phpunit/phpunit.plugin.zsh
@@ -1,19 +1,11 @@
 # Phpunit: list groups basic command completion
 
 _phpunit_get_group_list () {
-<<<<<<< HEAD
     phpunit --list-groups | sed "1,/Available test group(s):/d" | awk '/[a-z]+/ { print $2 }' |more
 }
 _phpunit_get_cmd_list () {
     phpunit --help |grep "-" |awk '/[a-z]+/ { print $1 }'
-=======
-    phpunit --list-groups | sed "1,/Available test group(s):/d" | awk '/[a-z]+/ { print $2 }'
->>>>>>> 989dc4a81f9d78cdf3259ea465a241eb390a3f7b
 }
-_phpunit_get_cmd_list () {
-    phpunit --help |grep "-" |awk '/[a-z]+/ { print $1 }'
-}
-
 _phpunit_group () {
     compadd `_phpunit_get_group_list`
 }
@@ -25,7 +17,3 @@ _phpunit() {
 compdef _phpunit_group phpunit
 
 alias phpunit-group='phpunit --group'
-<<<<<<< HEAD
-
-=======
->>>>>>> 989dc4a81f9d78cdf3259ea465a241eb390a3f7b
